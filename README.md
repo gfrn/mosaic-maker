bb9567c0fbfe038b45ff5b186bb155c90c6753f8
gfrn <guifreitas89@hotmail.com>
2025-02-15 16:52:05 +0000

    Update docs

--- README.md
+++ README.md
@@ -9,3 +9,39 @@ Source          | <https://github.com/gfrn/mosaicer>
 :---:           | :---:
 PyPI            | `pip install mosaicer`
 Releases        | <https://github.com/gfrn/mosaicer/releases>
+
+
+## Usage (command line)
+
+<img src="https://github.com/gfrn/mosaic-maker/docs/pepper.png" width=300 height=300/>
+
+*Picture of bell pepper formed by protein structures*
+
+### Installation
+
+`pip install .`
+
+### Options
+
+* --clusters: Number of clusters to use for KMeans clustering for dominant colour detection
+* --processes: Number of processes to use for multithreaded processing
+* --images: Location of images folder
+* --labels: Location of labels file (.json)
+* --colours: Location of colours file (.npy)
+* --piece_size: Mosaic piece size (in pixels)
+* --resize_ratio: Ratio between the number of images used to form the larger image, and the original pixel count
+* --out_file: Output image
+* --in_file: Input image
+* --no_repeat: Do not reuse images to form mosaic
+
+## Usage (library)
+
+Check the `DominantColourCalculator` class attribute docs for more information
+
+```py
+from mosaicer.dominant import DominantColourCalculator
+dcc = DominantColourCalculator(n_clusters=4)
+
+im = Image.open(IMAGE_PATH_HERE)
+dominant_colour = dcc.calculate(np.asarray(im))
+```
--- src/mosaicer/dominant.py
+++ src/mosaicer/dominant.py
@@ -29,7 +29,15 @@ class DominantColourCalculator:
     def labels(self, values: List[str]):
         self._labels = values
 
-    def calculate(self, image: np.ndarray):
+    def calculate(self, image: np.ndarray, cluster_index: int = 1):
+        """Calculate n-th most populous colour cluster
+        
+        Args:
+            image: Image data to parse, as Numpy array
+            cluster_index: Cluster to get average colour from. Starts from 0 (most populous)
+            
+        Returns:
+            Average colour of selected cluster (as 1, 3 Numpy array)"""
         clusters = KMeans(n_clusters=self.n_clusters)
         clusters.fit(image.reshape(-1,image.shape[-1]))
 
@@ -37,9 +45,16 @@ class DominantColourCalculator:
         counter = Counter(clusters.labels_)
         
         # Get second most populous cluster 
-        return clusters.cluster_centers_[sorted(counter.items())[1][0]]
+        return clusters.cluster_centers_[sorted(counter.items())[cluster_index][0]]
 
-    def calculate_array(self, images: Sequence[str]):
+    def calculate_array(self, images: Sequence[str]) -> dict[str, np.ndarray]:
+        """Load data and calculate dominant colour for all images in array
+        
+        Args:
+            images: Paths to images to process
+        
+        Returns:
+            dict object with image paths as keys, and the dominant colour as values."""
         images_dc = {}
         for image in images:
             im = Image.open(image)
@@ -48,6 +63,13 @@ class DominantColourCalculator:
         return images_dc
 
     def calculate_all(self, images_loc: str, colours_loc: str, labels_loc: str, processes: int, ):
+        """Calculate all colours in a directory, and save labels/colour values to files.
+        
+        Args:
+            images_loc: Location of folder with images to parse
+            colours_loc: File path for new colour data file
+            labels_loc: File path for new labels data file
+            processes: Number of processes to use for parallel processing"""
         images = [join(images_loc, f) for f in listdir(images_loc) if isfile(join(images_loc, f))]
         images_b = batched(images, 25)
         with multiprocessing.Pool(processes=processes) as pool:
@@ -61,8 +83,16 @@ class DominantColourCalculator:
         with open(labels_loc, 'w') as f:
             json.dump(list(dict_results.keys()), f)
 
-    def get_nearest_label(self, colour: np.ndarray, no_repeat: bool = False):
-        """Calculate minimum euclidian distance to 3D point"""
+    def get_nearest_label(self, colour: np.ndarray, no_repeat: bool = False) -> str:
+        """Get image whose dominant colour most closely matches the passed colour. 
+        This is done by calculating the minimum euclidian distance to each colour in 'colours'.
+        
+        Args:
+            colour: Colour to find
+            no_repeat: Do not reuse the matched image in the future
+            
+        Returns:
+            Path to image"""
         distances = np.sqrt(np.sum((self.colours-colour)**2,axis=1))
         i = np.where(distances==np.amin(distances))[0][0]
         label = self.labels[i]
